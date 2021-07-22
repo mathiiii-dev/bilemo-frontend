d8387ce5a31b879b2e6ac64aa394a12b2fcf2cc2
Mathias <mat.micheli99@gmail.com>
2021-07-22 16:32:43 +0200

    design: new design for error page

--- pages/error/403.vue
+++ pages/error/403.vue
@@ -1,45 +1,41 @@
 <template xmlns="http://www.w3.org/1999/html">
-  <b-container>
+  <b-container class="mt-5">
     <b-row>
-      <b-col md="12">
-        <div class="error-template">
-          <h1>Oops!</h1>
-          <h2>403 Access Denied</h2>
-          <div class="error-details">
-            <p v-if="message">{{ message }}</p>
-            <p v-else>Sorry, an error has occured, Requested page not found!</p>
-          </div>
-          <div class="error-actions">
-            <nuxt-link to="/panel">Retour</nuxt-link>
-          </div>
-        </div>
+      <b-col md="10" lg="8" class="mx-auto p-4 font">
+        <p class="bg-title">Access Denied</p>
+        <h1 class="mt-3 font-weight-bold">Oops! 403 Access Denied</h1>
+        <p class="lead lead-font-size">Sorry, an error has occured, you don't have permission!</p>
+        <p v-if="message" class="message-font-size">{{ message }}</p>
+        <nuxt-link to="/panel" class="btn-panel btn text-white float-right">Retour au panel</nuxt-link>
       </b-col>
     </b-row>
   </b-container>
 </template>
 
-<style>
-.error-template {
-  padding: 40px 15px;
-  text-align: center;
-}
-
-.error-actions {
-  margin-top: 15px;
-  margin-bottom: 15px;
-}
-
-.error-actions .btn {
-  margin-right: 10px;
-}
-</style>
-
 <script>
 export default {
   data() {
     return {
-      message: localStorage.getItem('message-403') ?? null
+      message: localStorage.getItem('message-403')
     }
   }
 }
 </script>
+
+<style>
+.bg-title {
+  background-color: #e5eaef;
+  border-radius: 10px;
+  padding: 7px;
+  display: inline;
+}
+h1 {
+  font-size: 400%;
+}
+.lead-font-size {
+  font-size: 200%;
+}
+.message-font-size {
+  font-size: 150%;
+}
+</style>
